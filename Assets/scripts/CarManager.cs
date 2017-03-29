57596cbe497a502503b8f619c617c2e1104ecd7b
dana <dana@nanigans.com>
2017-03-28 23:39:10 -0400

    added rewired

---
+++ Assets/scripts/CarManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class CarManager : MonoBehaviour {
+	public int playerId = 0; // The Rewired player id of this character
+	private Player player; // The Rewired Player
+	private Vector3 moveVector;
+
+	private Rigidbody rb;
+
+	void Awake() {
+		player = ReInput.players.GetPlayer(playerId);
+	}
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (player.GetAxis ("Thrust"));
+		rb.AddForce (player.GetAxis ("Thrust"), 0, 0);
+	}
+}
